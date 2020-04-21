4b217338a74b8686718fecf8573b42fa92097b03
spectralflux <simon.fullick@gmail.com>
2020-04-21 16:41:41 +0200

    Added initial test, and some initial code examples.

---
+++ src/main/test/com/spectralflux/aeon/scan/ScannerTest.java
@@ -0,0 +1,34 @@
+package com.spectralflux.aeon.scan;
+
+import com.spectralflux.aeon.error.ErrorHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class ScannerTest {
+
+    private ErrorHandler errorHandler;
+
+    @BeforeEach
+    void setup() {
+        errorHandler = new ErrorHandler();
+    }
+
+    @Test
+    void testHelloWorld() {
+        String source = "print('Hello, World!')\n";
+        Scanner scanner = new Scanner(errorHandler, source);
+
+        List<Token> tokens = scanner.scanTokens();
+
+        // just want to see what's in there for now
+        tokens.forEach(System.out::println);
+
+        assertFalse(tokens.isEmpty());
+        assertFalse(errorHandler.hadError());
+    }
+
+}
